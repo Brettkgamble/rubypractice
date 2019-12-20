3224e47433aef7c2aba82d1803fa2082dad64cd7
Brett Gamble <brettkgamble@gmail.com>
2019-12-20 14:18:50 -0700

    0003 student.rb

--- ruby_projects/oop_projects/student.rb
+++ ruby_projects/oop_projects/student.rb
@@ -1,15 +1,20 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, email, username, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
+  end
 
   def to_s
-    "First Name #{@firstname}"
+    "First Name: #{@first_name}, Last Name: #{@last_name}, email: #{email}, username: #{username}, password: #{password}"
   end
 end
 
-mashrur = Student.new
-#mashrur.first_name = "Mashrur"
-#puts mashrur
+mashrur = Student.new("Brett", "Gamble", "brett@test.com", "bkg1", "1234")
+john = Student.new("John", "Doe", "doe@test.com", "Doe1", "1234")
+puts mashrur
+puts john
